613665594baf14375ec9e507b40dcecea8d0ab70
Bernhard Bein <bbein@senckenberg.de>
2025-11-13 22:23:18 +0100

    added folder with snakemake scripts

---
+++ scripts/read_stats.R
@@ -0,0 +1,278 @@
+library(tidyverse)
+library(ggplot2)
+library(data.table)
+library(RColorBrewer)
+library(scales)
+library(patchwork)
+
+labels <- c("BiozymB7","DeCodeHiFi","Equinox","InvitrogenPlatinum","KODXtreme","PacBioHiFiA","PacBioHiFiB","Taqara","longAmpTaq","repliQa", "All Libs combined")
+
+## Get number of colors for number of labels
+palette_colors <- brewer.pal(length(labels), "Paired")
+
+## Assign colors to labels
+custom_colors <- setNames(palette_colors, labels)
+
+## Input for plotting/sumstats
+input_path_all = snakemake@params[["stats_path"]]
+input_names = snakemake@params[["sample_names"]]
+in_colors = snakemake@params[["colors"]]
+
+## output (N(x) + compleasm tables, final plot)
+gc_hist_hist_out = snakemake@output[["gc_hist_hist"]]
+qv_plot_out = snakemake@output[["qv_plot"]]
+standard_hist_plot_readeval_out = snakemake@output[["standard_hist_plot_readeval"]]
+rdeval_like_out = snakemake@output[["rdeval_like"]]
+
+
+## set colors for names
+input_names <- c(input_names)
+#input_names <- unlist(strsplit(input_names, split = ","))
+if (!is.null(input_names)){ #input_names[1] != ""
+  labels <- input_names
+  if (in_colors != "") {
+  col_dict <- read_delim(in_colors, stringsAsFactors = FALSE, col_names = FALSE)
+  custom_colors <- setNames(col_dict$color, col_dict$name)
+  } else {
+  palette_colors <- colorRampPalette(brewer.pal(8, "Set2"))(length(labels))
+  custom_colors <- setNames(palette_colors, labels)
+}
+}
+
+## find breakpoints
+get_breaks <- function(min, max, steps = c(5,10)) {
+  logstart <- floor(log10(min))
+  logend <- ceiling(log10(max))
+  
+  breaks <- c()
+  for (e in logstart:logend) {
+  breaks <- c(breaks, steps*10^e)
+  }
+  
+  return(breaks)
+}
+
+
+## Function to read and manipulate a rdeval dumped sequence report tsv file
+read_rdeval <- function(path, name){
+  
+  rdeval_in <- fread(path, header = T)
+  
+  rdeval_in_named  <- rdeval_in %>%
+    mutate(polymerase = name)
+  
+  gc_bins_hist <- rdeval_in_named %>%
+    mutate(GC_bin = round(GC,1)) %>%
+    group_by(Length, GC_bin) %>%
+    summarize(num_reads = n()) %>%
+    ungroup() %>%
+    group_by(GC_bin) %>%
+    mutate(relative_contribution = num_reads/sum(num_reads),
+           mean_rl = sum(Length*num_reads)/sum(num_reads),
+           median_rl = median(rep(Length,num_reads)),
+           polymerase = name) %>%
+    filter(sum(num_reads) >= 100)
+  
+  gc_bins_dens <- rdeval_in_named %>%
+    mutate(GC_bin = round(GC,1)) %>%
+    group_by(GC_bin) %>%
+    mutate(mean_rl = mean(Length),
+           median_rl = median(Length),
+           polymerase = name) %>%
+    filter(n() >= 100)
+  
+  m <- rdeval_in_named %>%
+  count(Length) %>%
+  summarise(max_value = max(n, na.rm = TRUE))
+  
+  scatter_breaks <- get_breaks(1, m$max_value)
+  
+  scatter <- ggplot(rdeval_in_named, aes(Length, `Average Quality`)) +
+  geom_bin_2d(bins=100, binwidth = c(1, 1)) +
+  theme_bw() +
+  scale_fill_gradient(
+    trans = "log10",
+    low = "lightblue",
+    high = "purple",
+    name = "Count (log10)",
+    breaks = scatter_breaks,
+    label = comma) + 
+  theme(legend.position = "left") +
+  xlim(c(-1, mean(rdeval_in_named$Length) + 4*sd(rdeval_in_named$Length))) 
+  
+  print(max(rdeval_in_named$`Average Quality`))
+  
+  read_length_histogram <- ggplot(rdeval_in_named, aes(Length)) +
+    geom_histogram(binwidth = 1, aes(fill = polymerase)) +
+    scale_fill_manual(values = custom_colors) + 
+    theme_void() +
+    xlim(c(-1, mean(rdeval_in_named$Length) + 4*sd(rdeval_in_named$Length)))
+    
+  
+  qc_histogram <- ggplot(rdeval_in_named, aes(`Average Quality`)) +
+    geom_histogram(bins = 50, aes(fill = polymerase)) +
+    scale_fill_manual(values = custom_colors) +
+    theme_void() +
+    coord_flip()
+    #xlim(c(0,50)) 
+
+  
+  combined <- read_length_histogram + plot_spacer() + scatter + qc_histogram + 
+    plot_layout(
+      ncol = 2, 
+      nrow = 2, 
+      widths = c(4, 1),
+      heights = c(1, 4),
+      guides = "collect"
+    )
+  
+  out_list <- list()
+  out_list[[1]] <- rdeval_in_named
+  out_list[[2]] <- gc_bins_hist
+  out_list[[3]] <- gc_bins_dens
+  out_list[[4]] <- combined
+    
+  return(out_list)
+}
+
+## create rdeval plots
+rdeval_plots <- function(path) {
+  
+  ## set up variables
+  tmp_path <- paste(path, "*rdeval_dump.tsv", sep="/")
+  in_files <- Sys.glob(tmp_path)
+
+  rdeval_plots <- list()
+  rdeval_gc_bins <- list()
+  rdeval_gc_dens <- list()
+  rdeval_standard <- list()
+  
+  ## read files
+  for (i in seq_along(in_files)) {
+    pattern <- str_extract(in_files[i], paste(labels, collapse="|"))
+    if (!is.na(pattern)) {
+    tmp_frame <- read_rdeval(in_files[i], name=pattern)
+
+    rdeval_plots[[i]] <- tmp_frame[[4]]
+    rdeval_gc_bins[[i]] <- tmp_frame[[2]]
+    rdeval_gc_dens[[i]] <- tmp_frame[[3]]
+    rdeval_standard[[i]] <- tmp_frame[[1]]
+  }
+  }
+  
+  gc_bin_frame <- rbindlist(rdeval_gc_bins)
+  gc_dens_frame <- rbindlist(rdeval_gc_dens)
+  standard_frame <- rbindlist(rdeval_standard)
+  
+  ## plot
+  
+  dens_plot <- ggplot(gc_dens_frame, aes(Length)) +
+    geom_density(aes(fill = polymerase), alpha = 0.6) + 
+    geom_vline(aes(xintercept = mean_rl), colour = 'black', size = 0.75) +
+    geom_vline(aes(xintercept = median_rl), colour = 'grey', size = 0.75, linetype = "dashed") +
+    facet_grid(GC_bin ~ polymerase) +
+    scale_fill_manual(values = custom_colors) +
+    theme_bw()
+    
+  standard_frame <- standard_frame %>%
+  group_by(polymerase) %>%
+  mutate(mean_length = mean(Length),
+            median_length = median(Length))
+
+  standard_hist_plot <- ggplot(standard_frame, aes(Length)) +
+    geom_histogram(binwidth = 1, aes(fill = polymerase)) +
+    scale_fill_manual(values = custom_colors) +
+    geom_vline(aes(xintercept = mean_length), colour = 'black', size = 0.75) +
+    geom_vline(aes(xintercept = median_length), colour = 'grey', size = 0.75, linetype = "dashed") +
+    facet_grid(rows = vars(polymerase)) +
+    theme_bw() +
+    xlim(c(-1, mean(standard_frame$Length) + 4*sd(standard_frame$Length)))
+    
+  #rdeval_like <- patchwork::wrap_plots(rdeval_plots, 
+  #                                nrow = 2, ncol = 5)
+  
+  out_list <- list()
+  out_list[[1]] <- dens_plot
+  out_list[[2]] <- standard_hist_plot
+  out_list[[3]] <- rdeval_plots
+  
+  return(out_list)
+}
+
+## create qv plots
+qv_plots <- function(path){
+  
+  read_qv <- function(file_path, name) {
+    qv_file <- read_delim(file_path, col_names = T)
+    qv_file <- qv_file %>%
+      filter(fraction1 > 0) %>%
+      mutate(polymerase = name)
+  }
+  
+  ## set up variables
+  tmp_path <- paste(path, "*qchist.txt", sep="/")
+  in_files <- Sys.glob(tmp_path)
+  qv_list <- list()
+  
+  ## read files
+  for (i in seq_along(in_files)) {
+    pattern <- str_extract(in_files[i], paste(labels, collapse="|"))
+    if (!is.na(pattern)) {
+    tmp_frame <- read_qv(in_files[i], name=pattern)
+    qv_list[[i]] <- tmp_frame
+  }
+  }
+  
+  qv_frame <- rbindlist(qv_list)
+  
+  ## plot
+  
+  qv_plot <- ggplot(qv_frame, aes(polymerase, fraction1)) +
+    geom_bar(aes(fill = factor(`#Quality`)), position="stack", stat="identity") +
+    scale_fill_viridis_d(name = "QV",direction = -1) +
+    theme_bw() +
+    ylab("percentage of reads")
+  
+  ## output
+  qv_list <- list()
+  qv_list[[1]] <- qv_plot
+  qv_list[[2]] <- qv_frame
+  
+  return(qv_list)
+
+}
+
+## create output plots
+
+## qv plot
+qv_out <- qv_plots(input_path_all)
+qv_plot <- qv_out[[1]]
+
+## rdeval
+rdeval_out <- rdeval_plots(input_path_all)
+
+## Standard hist (rdeval)
+standard_hist_plot_readeval <- rdeval_out[[2]]
+
+## GC hist (rdeval)
+gc_hist_hist <- rdeval_out[[1]]
+
+## Rdeval-like
+rdeval_like <- rdeval_out[[3]]
+  
+## write output
+ggsave(qv_plot_out, plot = qv_plot, device = 'pdf', width = 12, height = 14, units = "in")
+ggsave(standard_hist_plot_readeval_out, plot = standard_hist_plot_readeval, device = 'pdf', width = 12, height = 14, units = "in")
+ggsave(gc_hist_hist_out, plot = gc_hist_hist, device = 'pdf', width = 12, height = 14, units = "in")
+
+
+# Open PDF device
+pdf(rdeval_like_out, width = 7, height = 7)
+
+# Print each plot on a new page
+for (p in rdeval_like) {
+  print(p)
+}
+
+# Close the device to write file
+dev.off()
---
+++ scripts/ref_free_summary_combine.R
@@ -0,0 +1,487 @@
+## Figure code for Figure 1 of polymerase benchmark paper
+library(tidyverse)
+library(ggplot2)
+library(data.table)
+library(RColorBrewer)
+library(scales)
+library(patchwork)
+
+labels <- c("BiozymB7","DeCodeHiFi","Equinox","InvitrogenPlatinum","KODXtreme","PacBioHiFiA","PacBioHiFiB","Taqara","longAmpTaq","repliQa")
+## Get number of colors for number of labels
+palette_colors <- brewer.pal(length(labels), "Paired")
+
+## Assign colors to labels
+custom_colors <- setNames(palette_colors, labels)
+custom_colors
+
+## Input for plotting/sumstats
+input_path_faidx = snakemake@params[["faidx_path"]]
+input_path_compleasm = snakemake@params[["compleasm_path"]]
+compleasm_database_name = snakemake@params[["compleasm_database"]]
+input_path_merqury = snakemake@params[["merqury_path"]]
+input_path_hifieval = snakemake@params[["hifieval_path"]]
+input_names = snakemake@params[["sample_names"]]
+in_colors = snakemake@params[["colors"]]
+
+## output (N(x) + compleasm tables, final plot)
+output_ng_table = snakemake@output[["ng_table"]]
+output_compleasm_table = snakemake@output[["compleasm_table"]]
+output_final_figure = snakemake@output[["final_plot"]]
+out_hifieval_table = snakemake@output[["hifieval_table"]]
+out_merqury_table = snakemake@output[["merqury_table"]]
+
+#input_path_faidx <- args[1]
+#input_path_compleasm <- args[2]
+#compleasm_database_name <- args[3]
+#input_path_seqkit_stats <- args[4]
+#input_path_merqury <- args[5]
+#input_path_hifieval <- args[6]
+#input_names <- args[7]
+#in_colors <- args[8]
+#output_ng_table <- args[9]
+#output_compleasm_table <- args[10]
+#output_final_figure <- args[11]
+#out_hifieval_table <- args[12]
+#out_merqury_table <- args[13]
+
+## Check whether colors are present (Format is name, color):
+#col_dict <- read_delim(in_colors, stringsAsFactors = FALSE, col_names = FALSE)
+#custom_colors <- setNames(col_dict$color, col_dict$name)
+
+## set colors for names
+input_names <- c(input_names)
+#input_names <- unlist(strsplit(input_names, split = ","))
+if (!is.null(input_names)){ #input_names[1] != ""
+  labels <- input_names
+  if (in_colors != "") {
+  col_dict <- read_delim(in_colors, stringsAsFactors = FALSE, col_names = FALSE)
+  custom_colors <- setNames(col_dict$color, col_dict$name)
+  } else {
+  palette_colors <- colorRampPalette(brewer.pal(8, "Set2"))(length(labels))
+  custom_colors <- setNames(palette_colors, labels)
+}
+}
+
+
+## format labels for N50 plot
+format_labels <- function(x) {
+  sapply(x, function(val) {
+    if (val >= 1e9){
+      paste0(val / 1e9, "Gb")
+    }  
+      else if (val >= 1e6) {
+      paste0(val / 1e6, "Mb")
+    } else if (val >= 1e3) {
+      paste0(val / 1e3, "Kb")
+    } else {
+      paste0(val, "B")
+    }
+  })
+}
+
+## find breakpoints
+get_breaks <- function(min, max, steps = c(1,2.5,5)) {
+  logstart <- floor(log10(min))
+  logend <- ceiling(log10(max))
+  
+  breaks <- c()
+  for (e in logstart:logend) {
+  breaks <- c(breaks, steps*10^e)
+  }
+  
+  labels <- format_labels(breaks)
+  
+  break_list <- list()
+  break_list[[1]] <- breaks
+  break_list[[2]] <- labels
+  
+  return(break_list)
+}
+
+## contig N50 plots for all polymerases
+## load chrom size plots
+prepare_n50 <- function(chrom_file_path = "", asm = "") {
+  chrom_file <- read_delim(chrom_file_path, col_names = F)
+  
+  chrom_file_ord <- chrom_file %>%
+    select(X1,X2) %>%
+    arrange(desc(X2)) %>%
+    rename(length = X2,
+           chr_ID = X1) %>%
+    mutate(cumul_size = cumsum(length),
+           n_val = round((cumul_size/sum(length) *100), 3),
+           order_val = as.integer((row_number()) )) %>%
+    rename(setNames("cumul_size", paste("cumul_size", asm, sep = ".")),
+           setNames("length", paste("length", asm, sep = ".")),
+           setNames("n_val", paste("n_val", asm, sep = ".")),
+           setNames("chr_ID", paste("chr_ID", asm, sep = ".")))
+  
+  return(chrom_file_ord)
+}
+
+## do the N50 plot and summary
+n50_summary <- function(path) {
+  
+  ## set up variables
+  tmp_path <- paste(path, "*.fa.fai", sep="/")
+  in_files <- Sys.glob(tmp_path)
+  chrom_list <- list()
+  
+  ## read files
+  for (i in seq_along(in_files)) {
+    pattern <- str_extract(in_files[i], paste(labels, collapse="|"))
+    
+    if (!is.na(pattern)) {
+    chrom_list[[i]] <- prepare_n50(in_files[i], asm=pattern)
+  } 
+  }
+   
+  chrom_list <- chrom_list[lengths(chrom_list) > 0]
+  
+  ## convert to dataframe
+  chrom_df <- chrom_list %>% 
+    reduce(full_join, by='order_val') %>%
+    pivot_longer(-order_val, 
+                 names_to = c(".value", "Assembly"), 
+                 names_sep='\\.')
+                 
+                 
+  ## get contig N50 values
+  n50_info <- chrom_df %>%
+    mutate(n50_proxy = abs(n_val - 50)) %>%
+    group_by(Assembly) %>%
+    filter(n50_proxy  == min(n50_proxy, na.rm = T)) %>%
+    ungroup()
+    
+  ## get top 10 assemblies
+  top_10 <- n50_info %>%
+    slice_max(order_by = length, n = 10)
+  
+  print(top_10)
+  
+  chrom_df <- chrom_df %>%
+  filter(Assembly %in% top_10$Assembly)          
+                                
+                 
+  ## get break points for plot
+  break_list <- get_breaks(min(n50_info$length), max(n50_info$length))
+  
+  ## create N50 plot
+  n50_graph_plot <- ggplot(chrom_df, aes(n_val, length, color = Assembly)) + #,size = Assembly 
+    geom_step(size = 0.4) +
+    theme_bw() +
+    theme(axis.text= element_text(size = 15),
+          axis.title= element_text(size = 18),
+          legend.position = "none") + 
+    scale_y_log10(
+      breaks = break_list[[1]],
+      label = break_list[[2]],
+      limits = c(1000,max(chrom_df$length))) +
+    annotation_logticks(sides = "l") +  
+    scale_x_continuous(breaks = c(0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100) ) +
+    ylab("Contig length (bp)") +
+    xlab("N(x) value") +
+    geom_vline(xintercept = 50, color = "red",linetype = "dashed") +
+    scale_color_manual(values = custom_colors)
+  
+  
+  ## write output list 
+  n50_out_list <- list()
+  n50_out_list[[1]] <- n50_info
+  n50_out_list[[2]] <- n50_graph_plot
+  n50_out_list[[3]] <- top_10
+  return(n50_out_list)
+  
+}
+
+## compleasm summary
+read_compl <- function(path, name) {
+  compl <- read_delim(path, col_names = F)
+  compl <- compl %>%
+    rename(BUSCO_class = X1) %>%
+    mutate(polymerase = name)
+  return (compl)
+}
+
+## create summary stats and output plot
+compleasm_summary <- function(path, compleasm_label, top_10) {
+  
+  ## set up variables
+  tmp_path <- paste(path, "*rf.txt", sep="/")
+  in_files <- Sys.glob(tmp_path)
+  compl_list <- list()
+  
+  ## create data table
+  for (i in seq_along(in_files)) {
+    pattern <- str_extract(in_files[i], paste(labels, collapse="|"))
+    if (!is.na(pattern)) {
+    compl_list[[i]] <- read_compl(in_files[i], name=pattern)
+  }
+  }
+  
+  
+  compl_list <- compl_list[lengths(compl_list) > 0]
+  
+  #compl_list <- compl_list[lengths(compl_list) > 0]
+  compl_all <- rbindlist(compl_list)
+  compl_all <- compl_all %>%
+  filter(polymerase %in% top_10$Assembly)
+  
+  compl_all$category <- factor(compl_all$BUSCO_class, levels = c("M","I","F","D","S"), ordered = TRUE)
+  
+  ## color scheme
+  custom_colors_s <- c("S" = "#274655",
+                       "D" = "#2A9D8E",
+                       "F" = "#E9C46B",
+                       "I" = "#E9C48D",
+                       "M" = "#E56F52")
+                    
+                    
+  compl_all <- compl_all %>%
+  mutate(wrapped_polymerase = str_replace_all(polymerase, "PLUS", "\\+\n")) 
+  
+  sel_order_compl <- 
+    compl_all%>% 
+    group_by(wrapped_polymerase) %>%
+    mutate(
+      "SD" = sum(X3[category %in% c("S", "D")])
+    ) %>%
+    arrange(SD) %>% 
+    filter(category == "S") %>% 
+    mutate(wrapped_polymerase = factor(wrapped_polymerase))
+                       
+  compl_all_mut <- compl_all%>% 
+    mutate(wrapped_polymerase = factor(wrapped_polymerase, levels = sel_order_compl$wrapped_polymerase, ordered = TRUE))
+  
+  compleasm_plot <- ggplot(compl_all_mut, aes(X3, wrapped_polymerase, fill = category)) +
+    geom_col() +
+    theme_bw() +
+    theme(axis.text= element_text(size = 6),
+          axis.title= element_text(size = 12))+
+    scale_fill_manual(name = "compleasm status", values = custom_colors_s, 
+                      labels =  c("missing", "fragmented(split)", "fragmented(partial)", "complete (duplicated)", "complete (single)")) +
+    xlab(compleasm_label) +
+    ylab("")
+  
+  ## Compleasm Summary
+  compl_all_out_table <- compl_all_mut %>%
+    group_by(polymerase) %>%
+    pivot_wider(names_from=BUSCO_class,values_from = c(X3,X2))%>%
+    summarise(across(everything(), ~ max(.x, na.rm = TRUE))) %>%
+    mutate(sum_intact = X3_S + X3_D,
+           perc_intact = X2_S + X2_D,
+           sum_all = X3_S + X3_D + X3_F + X3_I + X3_M) %>%
+    select(-category, -wrapped_polymerase)
+
+  compleasm_out_list <- list()
+  
+  compleasm_out_list[[1]] <- compl_all_out_table
+  compleasm_out_list[[2]] <- compleasm_plot
+  
+  ## return outlist
+  return(compleasm_out_list)
+  
+}
+
+
+output_hifieval_readstats <- function(path, top_10)
+{
+  ## set up variables
+  tmp_path <- paste(path, "*.summary.tsv", sep="/")
+  in_files <- Sys.glob(tmp_path)
+  hifieval_list <- list()
+  
+  read_hifieval <- function(path, name) {
+    hifieval_reads <- fread(path, header = T)
+    hifieval_reads <- hifieval_reads %>%
+      mutate(polymerase = name)
+    return(hifieval_reads)
+  }
+  
+  ## read files
+  for (i in seq_along(in_files)) {
+    pattern <- str_extract(in_files[i], paste(labels, collapse="|"))
+    if (!is.na(pattern)){
+    hifieval_list[[i]] <- read_hifieval(in_files[i], name=pattern)
+    }
+  }
+  
+  ## convert to dataframe
+  hifieval_list <- hifieval_list[lengths(hifieval_list) > 0]
+  hifieval_df <- rbindlist(hifieval_list)
+
+
+  hifieval_df <- hifieval_df %>%
+  filter(polymerase %in% top_10$Assembly)
+  
+  ## collapse df
+  hifieval_df_sum <- hifieval_df %>%
+    group_by(polymerase) %>%
+    summarise(corrected_bases = sum(num_cc)/sum(raw_end - raw_start),
+              undercorrected_bases = sum(num_uc)/sum(raw_end - raw_start),
+              overcorrected_bases = sum(num_oc)/sum(raw_end - raw_start)) %>%
+    pivot_longer(cols = c(corrected_bases,
+                          undercorrected_bases,
+                          overcorrected_bases), 
+                 names_to = "correction_class",
+                 values_to = "fraction") 
+  
+  ## Adjust labels
+  hifieval_df_sum <- hifieval_df_sum %>%
+    mutate(wrapped_polymerase = str_replace_all(polymerase, "PLUS", "\\+\n"))
+  
+  ## plot
+  hifieval_plot <- ggplot(hifieval_df_sum, aes(polymerase, fraction, fill = polymerase)) +
+    geom_col(position = "dodge2") +
+    scale_y_continuous(labels = comma) +
+    scale_fill_manual(values = custom_colors,
+    name = "polymerase",
+    labels = unique(hifieval_df_sum$wrapped_polymerase))  +
+    theme_bw() +
+    theme(axis.text.x = element_blank()) + 
+    ylab("% of corrected bases")
+   
+  hifieval_out <- list()
+  
+  ## Write output
+  hifieval_out[[1]] <- hifieval_plot 
+  hifieval_out[[2]] <- hifieval_df_sum
+  
+  return(hifieval_out)
+}
+
+
+merqury_asm_sum <- function(path, top_10)
+{
+
+
+  read_merqury_oneline <- function(path, name) {
+    merqury <- read_delim(path, col_names = F)
+    merqury <- merqury %>%
+      mutate(polymerase = name)
+    return(merqury)
+  }
+  
+  ## set up variables
+  tmp_path_qv <- paste(path, "*/*slf.qv", sep="/")
+  in_files_qv <- Sys.glob(tmp_path_qv)
+  tmp_path_com <- paste(path, "*/*completeness.stats", sep="/")
+  in_files_com <- Sys.glob(tmp_path_com)
+  merqury_list_qv <- list()
+  merqury_list_com <- list()
+  
+  ## create data table
+  for (i in seq_along(in_files_qv)) {
+    pattern <- str_extract(in_files_qv[i], paste(labels, collapse="|"))
+    if (!is.na(pattern)) {
+    merqury_list_qv[[i]] <- read_merqury_oneline(in_files_qv[i], name=pattern)
+  }
+  }
+  
+  for (i in seq_along(in_files_com)) {
+    pattern <- str_extract(in_files_com[i], paste(labels, collapse="|"))
+    if (!is.na(pattern)) {
+    merqury_list_com[[i]] <- read_merqury_oneline(in_files_com[i], name=pattern)
+  }
+  }
+  
+  merqury_list_qv <- merqury_list_qv[lengths(merqury_list_qv) > 0]
+  mq_qv <- rbindlist(merqury_list_qv)
+  
+  merqury_list_com <- merqury_list_com[lengths(merqury_list_com) > 0]
+  mq_com <- rbindlist(merqury_list_com)
+  
+  merqury_all <- mq_qv %>%
+  left_join(mq_com, by = c("X1" = "X1")) %>%
+  rename(polymerase = X1)
+  
+  merqury_all <- merqury_all %>%
+  filter(polymerase %in% top_10$Assembly)
+    
+  er <- ggplot(merqury_all, aes(polymerase, X5.x)) +
+  geom_col(aes(fill = polymerase)) +
+  #ggtitle("Merqury assembly error rate") +
+  ylab("error rate") + 
+  theme_bw() +
+  scale_fill_manual(values = custom_colors) +
+    theme(axis.text.x= element_blank(),
+        axis.text.y= element_text(size = 10),
+        axis.title= element_text(size = 11))
+
+  qv <- ggplot(merqury_all, aes(polymerase, X4.x)) +
+  geom_col(aes(fill = polymerase)) +
+  coord_cartesian(ylim = c(min(merqury_all$X4.x)-5, 60)) +
+  #ggtitle("Merqury assembly quality value") +
+  ylab("QV") + 
+  theme_bw() +
+  scale_fill_manual(values = custom_colors) +
+    theme(axis.text.x= element_blank(),
+        axis.text.y= element_text(size = 10),
+        axis.title= element_text(size = 11))
+
+  comp <- ggplot(merqury_all, aes(polymerase, X5.y)) +
+  geom_col(aes(fill = polymerase)) +
+  coord_cartesian(ylim = c(min(merqury_all$X5.y)-2, 100)) +
+  #ggtitle("Merqury assembly completeness") +
+  ylab("Completeness (%)") +
+  theme_bw() +
+  scale_fill_manual(values = custom_colors) +
+  theme(axis.text.x= element_blank(),
+        axis.text.y= element_text(size = 10),
+        axis.title= element_text(size = 11))
+  
+  out_plot <- (er / qv /comp) +
+  plot_layout(guides = "collect",
+              axes = "collect") & theme(legend.position = "none")
+              
+  out_list <- list()
+  out_list[[1]] <- out_plot
+  out_list[[2]] <- merqury_all
+  
+  return(out_list)
+}
+
+## final output
+
+## N(x) plot (and table)
+n50_plot <- n50_summary(input_path_faidx)
+top_10 <- n50_plot[[3]]
+
+## compleasm plot (and table)
+compleasm_plot <- compleasm_summary(input_path_compleasm, 
+                                          compleasm_database_name, top_10)
+
+## hifieval error stats
+if (!is.null(input_path_hifieval)) {
+hifieval_out <- output_hifieval_readstats(input_path_hifieval, top_10)
+}
+
+## Merqury output plot
+merqury_out <- merqury_asm_sum(input_path_merqury, top_10)
+
+## Final Plot
+if (!is.null(input_path_hifieval)) {
+final_plot <- (
+  n50_plot[[2]] /
+  compleasm_plot[[2]] /
+  merqury_out[[1]] /
+   hifieval_out[[1]]) +
+   plot_layout(guides = "collect", heights = c(3,2,2,2)) + 
+  plot_annotation(tag_levels = 'A') 
+  } else { ## Final Plot without hifieval
+final_plot <- (
+  n50_plot[[2]] /
+  compleasm_plot[[2]] /
+  merqury_out[[1]]) +
+   plot_layout(guides = "collect", heights = c(3,2,2)) + 
+  plot_annotation(tag_levels = 'A')
+}
+
+## write output
+ggsave(output_final_figure, plot = final_plot, device = "pdf", width = 12, height = 14, units = "in")
+write_delim(n50_plot[[1]], output_ng_table, delim = "\t")
+write_delim(compleasm_plot[[1]], output_compleasm_table, delim = "\t")
+write_delim(merqury_out[[2]], out_merqury_table, delim = "\t")
+if (!is.null(input_path_hifieval)) {
+  write_delim(hifieval_out[[2]], out_hifieval_table, delim = "\t")
+  }
